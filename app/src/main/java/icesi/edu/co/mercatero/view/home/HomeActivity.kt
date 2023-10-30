841e42b2ddf01e04cffe827f7cf861a8f76f93dd
Brayan Ortiz~ <65927857+BrayanOB2003@users.noreply.github.com>
2023-10-30 15:34:03 -0500

    feat: create base of HomeActivity to test de transition screens

---
+++ app/src/main/java/icesi/edu/co/mercatero/view/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package icesi.edu.co.mercatero.view.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import icesi.edu.co.mercatero.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
