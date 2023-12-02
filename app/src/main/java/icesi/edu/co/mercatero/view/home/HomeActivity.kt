c9847bb81925189a992bb44dff4bd32b9f329019
Brayan Ortiz~ <65927857+BrayanOB2003@users.noreply.github.com>
2023-12-02 16:17:12 -0500

    feat: home navbar added

--- app/src/main/java/icesi/edu/co/mercatero/view/home/HomeActivity.kt
+++ app/src/main/java/icesi/edu/co/mercatero/view/home/HomeActivity.kt
@@ -2,6 +2,8 @@ package icesi.edu.co.mercatero.view.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
 import icesi.edu.co.mercatero.R
 import icesi.edu.co.mercatero.databinding.ActivityAuthBinding
 import icesi.edu.co.mercatero.databinding.ActivityHomeBinding
@@ -13,5 +15,34 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        loadFragment(HomeFragment())
+
+        binding.bottomNavigation.setOnItemSelectedListener { menuItem ->
+            handleNavigationItemSelected(menuItem)
+        }
+    }
+
+    private fun handleNavigationItemSelected(item: MenuItem): Boolean {
+        var selectedFragment: Fragment? = null
+
+        when (item.itemId) {
+            R.id.navigation_home -> selectedFragment = HomeFragment()
+            R.id.navigation_orders -> selectedFragment = OrdersFragment()
+            R.id.navigation_favorites -> selectedFragment = FavoritesFragment()
+            R.id.navigation_profile -> selectedFragment = ProfileFragment()
+        }
+        return try {
+            selectedFragment?.let { loadFragment(it) }
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
     }
 }
