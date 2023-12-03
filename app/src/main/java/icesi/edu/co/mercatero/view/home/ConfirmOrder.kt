2e00d7ee06462fa21b5e85a21884f7025fd4d8a8
GilmarAmezquita <1006351546@u.icesi.edu.co>
2023-12-03 18:27:47 -0500

    First steps in confirm order fragment

---
+++ app/src/main/java/icesi/edu/co/mercatero/view/home/ConfirmOrder.kt
@@ -0,0 +1,33 @@
+package icesi.edu.co.mercatero.view.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import icesi.edu.co.mercatero.databinding.FragmentConfirmOrderBinding
+import icesi.edu.co.mercatero.model.shop.Order
+
+class ConfirmOrder(orders: List<Order>) : Fragment() {
+
+    private lateinit var binding: FragmentConfirmOrderBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentConfirmOrderBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(orders: List<Order>) : ConfirmOrder {
+            return ConfirmOrder(orders)
+        }
+    }
+}
