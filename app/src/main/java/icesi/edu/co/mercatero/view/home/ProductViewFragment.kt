7c6817921731210ddc34467ee5bf5d78a599ca11
GilmarAmezquita <1006351546@u.icesi.edu.co>
2023-12-04 04:17:55 -0500

    Created view to add product in cart

---
+++ app/src/main/java/icesi/edu/co/mercatero/view/home/ProductViewFragment.kt
@@ -0,0 +1,63 @@
+package icesi.edu.co.mercatero.view.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import icesi.edu.co.mercatero.databinding.FragmentProductViewBinding
+import icesi.edu.co.mercatero.model.Product
+
+class ProductViewFragment(private var product: Product) : Fragment() {
+
+    private lateinit var binding: FragmentProductViewBinding
+    private var amount = 1
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProductViewBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.textViewProductName.text = product.name
+        binding.textViewProductDescription.text = product.description
+        binding.textViewProductPrice.text = product.price
+        binding.buttonDecrease.setOnClickListener {
+            decreaseAmount()
+        }
+        binding.buttonIncrease.setOnClickListener {
+            increaseAmount()
+        }
+        binding.buttonAddToCart.setOnClickListener {
+            addToCart()
+        }
+    }
+
+    private fun decreaseAmount() {
+        if (amount > 1) {
+            amount--
+            binding.textViewProductAmount.text = amount.toString()
+        }
+    }
+
+    private fun increaseAmount() {
+        amount++
+        binding.textViewProductAmount.text = amount.toString()
+    }
+
+    private fun addToCart() {
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(product: Product) : ProductViewFragment {
+            return ProductViewFragment(product)
+        }
+    }
+}
