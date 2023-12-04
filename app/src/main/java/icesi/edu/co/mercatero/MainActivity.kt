39ed4b8940848a0c27f2413bb4410227c1745012
Brayan Ortiz~ <65927857+BrayanOB2003@users.noreply.github.com>
2023-12-04 05:28:41 -0500

    feat: profile persistence and refactor

--- app/src/main/java/icesi/edu/co/mercatero/MainActivity.kt
+++ app/src/main/java/icesi/edu/co/mercatero/MainActivity.kt
@@ -3,35 +3,17 @@ package icesi.edu.co.mercatero
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import icesi.edu.co.mercatero.databinding.ActivityMainBinding
-import icesi.edu.co.mercatero.view.authentication.EntryFragment
-import icesi.edu.co.mercatero.view.authentication.SignUpFragment
-import icesi.edu.co.mercatero.view.home.MyProfile
+import icesi.edu.co.mercatero.view.authentication.AuthActivity
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var entryFragment: EntryFragment
-    lateinit var signUpFragment: SignUpFragment
     val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startActivity(Intent(binding.root.context, MyProfile::class.java))
+        startActivity(Intent(binding.root.context, AuthActivity::class.java))
     }
-
-    fun showFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-       // transaction.replace(binding.fragmentCV.id, fragment)
-        transaction.commit()
-    }
-    fun showFragment(fragment: Fragment, fragmentContainerID:Int) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(fragmentContainerID, fragment)
-        transaction.commit()
-    }
-
-
 }
